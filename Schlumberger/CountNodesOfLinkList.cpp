d6acec1b9206f55f6643289e6de2e350df2bbb97
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-13 06:20:18 +0530

    Update CountNodesOfLinkList.cpp

--- Schlumberger/CountNodesOfLinkList.cpp
+++ Schlumberger/CountNodesOfLinkList.cpp
@@ -7,3 +7,13 @@ class Solution
     return x;
     }
 };
+int getCount(struct Node* head,int k=0){
+    if(head==NULL)return 0;
+    int c=0;
+    Node*p=head;
+    while(p!=NULL)
+    {c++;
+        p=p->next;
+    }
+    return c;
+    }
