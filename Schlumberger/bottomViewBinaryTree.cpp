d118a15e51a0aa1692c6ff4321f8c0dac144df24
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2022-12-13 08:53:27 +0530

    Create bottomViewBinaryTree.cpp

---
+++ Schlumberger/bottomViewBinaryTree.cpp
@@ -0,0 +1,22 @@
+ vector <int> bottomView(Node *root) {
+      vector<int>v;
+      map<int,int>mp;
+      queue<pair<Node*,int>>q;
+      q.push({root,0});
+      while(!q.empty())
+      {
+          auto temp=q.front();
+          q.pop();
+          int hd=temp.second;
+          Node *node=temp.first;
+          mp[hd]=node->data;
+          if(node->left)
+          q.push({node->left,hd-1});
+          if(node->right)
+          q.push({node->right,hd+1});
+      }
+      for(auto x:mp) 
+        v.push_back(x.second);
+      
+      return v;
+    }
