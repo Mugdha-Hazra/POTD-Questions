b2b1f3435733303bcdbb71f5fddfd9171255ec1d
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2024-09-04 22:58:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int nthStair(int n){
+		    if(n==1||n==2)
+		    return n;
+		    return (n/2)+1;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthStair(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
