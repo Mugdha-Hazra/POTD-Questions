9dc1558645df0ad4236cafec39b56e14575a74b8
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2024-09-10 21:52:57 +0530

    Create A. Minimize!.cpp

---
+++ CFTracker/A. Minimize!.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int a,b;
+       cin>>a>>b;
+       cout<<b-a<<"\n";
+   }
+    return 0;
+}
